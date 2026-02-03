7ac07ea7e5ace3450254334492fa50595485bbd6
Fenrigore <noskovkg@yandex.ru>
2026-02-03 17:50:46 +0500

    Решение урока 14-2-6

--- MyVector.h
+++ MyVector.h
@@ -4,6 +4,70 @@
 #include <new>
 #include <utility>
 
+
+template <typename T>
+class RawMemory {
+public:
+	RawMemory() = default;
+
+	explicit RawMemory(size_t capacity)
+		:buffer_{ Allocate(capacity) }
+		, capacity_{capacity} {}
+
+	~RawMemory() {
+		Deallocate(buffer_);
+	}
+
+	 T* operator+(size_t offset) noexcept {
+		 // Разрешается получать адрес ячейки памяти, следующей за последним элементом массива
+		 assert(offset <= capacity_);
+		 return buffer_ + offset;
+	 }
+
+	 const T* operator+(size_t offset) const noexcept {
+		 return const_cast<RawMemory&>(*this) + offset;
+	 }
+
+	 T& operator[](size_t index) noexcept {
+		 assert(index < capacity_);
+		 return buffer_[index];
+	 }
+
+	 const T& operator[](size_t index) const noexcept {
+		 return const_cast<RawMemory&>(*this)[index];
+	 }
+
+	 void Swap(RawMemory& other) noexcept {
+		 std::swap(buffer_, other.buffer_);
+		 std::swap(capacity_, other.capacity_);
+	 }
+
+	 const T* GetAdress() const noexcept {
+		 return buffer_;
+	 }
+
+	 T* GetAdress() noexcept {
+		 return buffer_;
+	 }
+
+	 size_t Capacity() const noexcept {
+		 return capacity_;
+	 }
+
+private: //методы
+	static T* Allocate(size_t n) {
+		return n !=0 ? static_cast<T*>(operator new(sizeof(T) * n)):nullptr;
+	}
+
+	static void Deallocate(T* buf) noexcept {
+		operator delete(buf);
+	}
+
+private: //поля
+	T* buffer_{};
+	size_t capacity_{};
+};
+
 template <typename T>
 class Vector {
 public:
@@ -12,10 +76,8 @@ public:
 
 	//Конструктор вектора размером size с нулевыми значениями T
 	explicit Vector(size_t size) 
-		:data_(Allocate(size))
-		, capacity_(size)
+		:data_(size)
 		, size_(size) {
-
 		size_t i{};
 		try {
 			for (; i != size; ++i) {
@@ -25,9 +87,7 @@ public:
 		catch (...) {
 			// i хранит количество созданных объектов
 			//теперь их надо уничтожить
-			DestroyN(data_, i);
-			//и освободить память
-			Deallocate(data_);
+			DestroyN(data_.GetAdress(), i);
 			//перевыбрасываем исключение
 			throw;
 		}
@@ -36,8 +96,7 @@ public:
 	//конструктор копирования
 	Vector(const Vector& other)
 		//забронировать память размером как в other.size_
-		:data_(Allocate(other.size_))
-		, capacity_(other.size_)
+		:data_(other.size_)
 		, size_(other.size_){
 		
 		//логика выбрасывания исключения такая же как и в конструкторе с size
@@ -45,46 +104,44 @@ public:
 		try {
 			//скопировать в ячейку data_ + i объект из other.data_ по индексу i
 			for (; i < size_; ++i) {
-				CopyConstruct(data_ + i, other.data_[i]);
+				CopyConstruct((data_.GetAdress() + i), other.data_[i]);
+				//попробовать CopyConstruct(data_[i], other.data_[i]);
 			}
-
 		}
 		catch (...) {
-			DestroyN(data_, i);
-			Deallocate(data_);
+			DestroyN(data_.GetAdress(), i);
 			throw;
 		}
 	}
 
 	//Деструктор 
 	~Vector() {
-		DestroyN(data_, size_);
-		Deallocate(data_);
+		DestroyN(data_.GetAdress(), size_);
 	}
 
 	void Reserve(size_t new_capacity) {
-		if (new_capacity < capacity_) {
+		if (new_capacity < data_.Capacity()) {
 			return;
 		}
-		T* new_data = Allocate(new_capacity); //Если выбросит исключение то MyVector не изменится
+		RawMemory<T> new_data (new_capacity); //Если выбросит исключение то MyVector не изменится
 
 		//а тут как в конструкторе копирования
 		size_t i{};
 		try {
 			for (; i != size_; ++i) {
-				CopyConstruct(new_data + i, data_[i]);
+				CopyConstruct(new_data.GetAdress() + i, data_[i]);
 			}
 		}
 		catch (...) {
-			//тут уде уничтожаем во временной выделенной памяти
-			DestroyN(new_data, i);
-			Deallocate(new_data);
+			//тут уже уничтожаем во временной выделенной памяти
+			DestroyN(new_data.GetAdress(), i);
 			throw;
 		}
-		DestroyN(data_, size_);
-		Deallocate(data_);
-		data_ = new_data;
-		capacity_ = new_capacity;
+		//если было брошено исключение то код ниже не отработает уже
+		//затем свапаем через метод RawData
+		data_.Swap(new_data);
+		//и избавляемся от улик
+		DestroyN(new_data.GetAdress(), size_);
 	}
 
 	size_t Size() noexcept {
@@ -92,7 +149,7 @@ public:
 	}
 
 	size_t Capacity() noexcept {
-		return capacity_;
+		return data_.Capacity();
 	}
 
 	const T& operator [](size_t index) const noexcept {
@@ -104,18 +161,6 @@ public:
 		return data_[index];
 	}
 private: // приватные методы
-
-	//Резервирует память размером sizeof(T) * n
-	static T* Allocate(size_t n) {
-		//Если n больше ноля, то создаю резерву битов в памяти 
-		// и конвертирую этот указатель (void*) в указатель на шаблонный параметр T (T*)
-		return n != 0 ? static_cast<T*>(operator new (sizeof(T) * n)) : nullptr;
-	}
-
-	static void Deallocate(T* buf) noexcept {
-		operator delete(buf); //удаляю зарезервированную память (что с объектом?)
-	}
-
 	//Вызывает деструкторы n объектов массива по адресу buf
 	static void DestroyN(T* buf, size_t n) noexcept {
 		for (size_t i = 0; i < n; ++i) {
@@ -134,7 +179,6 @@ private: // приватные методы
 	}
 
 private: //приватные поля
-	T* data_{};
-	size_t capacity_{};
+	RawMemory<T> data_{};
 	size_t size_{};
 };
